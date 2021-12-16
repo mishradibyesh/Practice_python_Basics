6bca74c3837843c08a15484e787b2ea93c7d92f7
mishradibyesh <dibyeshmishra207@gmail.com>
2021-12-16 15:39:41 +0530

    ADDED : code to demonstrate the use of numeric data type

---
+++ numeric_data.py
@@ -0,0 +1,16 @@
+"""
+implementing basic data type problems
+"""
+number1 = 65
+number2 = 34
+print(f"sum of the numbers is {number1+number2}")
+print(type(number1+ number2))
+print(f"multiplication of the numbers is {number1 * number2}")
+print(type(number1 * number2))
+print(f"division of the numbers is {number1 / number2}")
+print(type(number1 / number2))
+print(f"Remainder of  the numbers is {number1 % number2}")
+print(type(number1 % number2))
+number3 = 6 + 7j
+print(type(number3))
+
