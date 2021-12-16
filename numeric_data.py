9ca415c2071ecf401104fa862fd85cc903dd6b32
mishradibyesh <dibyeshmishra207@gmail.com>
2021-12-16 15:50:27 +0530

    ADDED : code to learn numeric data types in python

--- numeric_data.py
+++ numeric_data.py
@@ -4,7 +4,7 @@ implementing basic data type problems
 number1 = 65
 number2 = 34
 print(f"sum of the numbers is {number1+number2}")
-print(type(number1+ number2))
+print(type(number1 + number2))
 print(f"multiplication of the numbers is {number1 * number2}")
 print(type(number1 * number2))
 print(f"division of the numbers is {number1 / number2}")
@@ -12,5 +12,8 @@ print(type(number1 / number2))
 print(f"Remainder of  the numbers is {number1 % number2}")
 print(type(number1 % number2))
 number3 = 6 + 7j
+number4 = 8 + 8j
+sum = number3 + number4
+print(f"the sum of complex numbers is {sum}")
 print(type(number3))
 
